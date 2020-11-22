a4fecba9cbba1baf1c47e93f4fadde41930e8744
carlosbelen <carlosbelen2004@hotmail.com>
2020-11-22 08:57:09 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Import for the shared module and ALL of its components
+import { SharedModule } from './shared/shared.module';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { FormsModule } from '@angular/forms';
+import { EditItemModalComponent } from './edit-item-modal/edit-item-modal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    LoginComponent,
+    BudgetItemListComponent,
+    BudgetItemCardComponent,
+    AddItemFormComponent,
+    EditItemModalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
